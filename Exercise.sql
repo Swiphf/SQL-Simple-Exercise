c25b1fb9c56cb10c3a49c26bffdb083470d83add
Your Name <you@example.com>
2023-05-25 20:20:42 +0300

    initial commit

---
+++ Exercise.sql
@@ -0,0 +1,32 @@
+--1, Show the first name, last name and telephone number for all the employees, except those who live in UK.
+--2, Show all product details for products whose unit price is greater than $10 and quantity in stock greater than 2.
+-- Sort by product price.
+--3, Show the first name, last name and telephone number for the employees who started working in the company in 1992-1993.
+--4, Show the product name, Company name of the supplier and stock quantity of the products that have 15,
+-- or more items in stock and the Product name starts with B or C or M.
+--5, Show all details for products whose Category Name is ' Meat/Poultry ' Or 'Dairy Products '. Sort them by product name.
+--6, Show Category name, Product name and profit for each product (how much money the company will earn if they sell all the units in stock).
+-- Sort by the profit.--7, Show the Employees' first name, last name and Category Name of the products which they have sold (show each category once).
+--7, Show the Employees' first name, last name and Category Name of the products which they have sold (show each category once).
+--8, Show the first name, last name, telephone number and date of birth for the employees who are aged older than 35.
+-- Order them by last name in descending order.
+--9, Show each employee’s name, the product name for the products that he has sold and quantity that he has sold.
+--10, Show for each order item – the customer name and order id, product name, ordered quantity, product price
+-- and total price (Ordered quantity * product price) and gap between ordered date and shipped date (the gap in days).
+-- Order by order id.
+--11, How much each customer paid for all the orders he had committed together?
+--12, In which order numbers was the ordered quantity greater than 10% of the quantity in stock?
+--13, Show how many Employees live in each country and their average age.
+--14 What would be the discount for all the London customers (together),
+--if after 5 days of gap between the order date and shipping date they get a 5% discount per item they bought?
+--15, Show the product id, name, stock quantity, price and total value (product price * stock quantity) 
+--for products whose total bought quantity is greater than 500 items.
+--16, For each employee display the total price paid on all of his orders that hasn’t shipped yet.
+--17, For each category display the total sales revenue, every year.
+--18, Which Product is the most popular? (number of items)
+--19, Which Product is the most profitable? (income)
+--20, Display products that their price higher than the average price of their Category.
+--21, For each city (in which our customers live), display the yearly income average.
+--22, For each month display the average sales in the same month all over the years.
+--23, Display a list of products and OrderID of the largest order ever placed for each product.
+--24, Display for each year, the customer who purchased the highest amount.
